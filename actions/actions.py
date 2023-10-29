cdde79fc58013a03ec46fbc69db55539769a6fb2
sherri-ice <sherri.ice7@gmail.com>
2023-10-29 23:32:29 +0300

    Added validation for location

--- actions/actions.py
+++ actions/actions.py
@@ -1,4 +1,4 @@
-from typing import Text, List, Dict
+from typing import Text, List, Dict, Any
 
 from rasa_sdk import Tracker, Action, ValidationAction
 from rasa_sdk.events import EventType
@@ -42,5 +42,33 @@ class AskForIsOpenAction(Action):
 
 
 # Actions for location processing
-# class ValidateLocationSlots(ValidationAction):
-#     def validate_
+
+# Mock-request for getting location's coordinates
+def mock_geo(location: str):
+    return 54, 34
+
+
+class ValidateLocationSlots(ValidationAction):
+    def validate_location(
+            self,
+            slot_value: List,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+
+        try:
+            # Get the latitude and longitude of the address by geocoding using geopy Nominatim
+            latitude, longitude = mock_geo(', '.join(slot_value))
+
+            if latitude is None:
+                # If the location isn't recognized then send the following message and set the slot value to none
+                dispatcher.utter_message(template="utter_wrong_address")
+                return {"location": None}
+            else:
+                # Otherwise keep the address slot value. Additionally, save the value of the coordinates to slot
+                return {"location": slot_value, 'coords': f'{latitude}, {longitude}'}
+        except:
+            # todo: define exception
+            # In case we run into an error we return the following message
+            dispatcher.utter_message(text='Facing server issues. Please try again later')
